af5e83cb94028a2193a8e60f92d62c8c706965ab
Sarah Neville <sarah.neville@students.fhv.at>
2024-11-20 19:23:14 +0100

    updated brick to display random math operation

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using TMPro;
 
 public class Brick : MonoBehaviour
 {
@@ -18,14 +19,39 @@ public class Brick : MonoBehaviour
     private string destroySound;
     [SerializeField]
     private PlayableDirector director;
+    [SerializeField]
+    private TextMeshPro mathValueTextBrick;
+    
     public bool IsPowerUp = false; //might need for adding powerups ...
-
     private int currentHitpoints;
+    private int mathValue;
+    private string mathOperator;
 
+
+    private enum MathOperatorsEnum
+    {
+        SUBTRACTION = 0,
+        ADDITION = 1,
+    }
     // Start is called before the first frame update
     void Start()
     {
+        MathOperatorsEnum randomOperator = (MathOperatorsEnum)UnityEngine.Random.Range(0, 2);
+        switch (randomOperator)
+        {
+            case MathOperatorsEnum.SUBTRACTION:
+                mathOperator = "-";
+                break;
+            case MathOperatorsEnum.ADDITION:
+                mathOperator = "+";
+                break;
+            default:
+                mathOperator = "+";
+                break;
+        }
         currentHitpoints = hitPoints;
+        mathValue = UnityEngine.Random.Range(1, 9);
+        mathValueTextBrick.text = mathOperator + mathValue.ToString();        
     }
 
     //Add force to ball when reflecting form brick if needed and invoke callback with damage number
@@ -75,6 +101,7 @@ public class Brick : MonoBehaviour
             destructionParticles.Play();
         }
         Destroy(gameObject, 4f); //Destroy game object after 4 seconds
+        mathValueTextBrick.text = "";     
     }
 
     private void DropPowerUp()
