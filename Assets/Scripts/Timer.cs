2f5ff3e53857dec4acaed17dbf09e9e021f0e32d
Sarah Neville <sarah.neville@students.fhv.at>
2024-12-02 22:56:21 +0100

    created timer prefab, script for timer, implementation not wokring yet

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text timerText;
+    public float timer;
+    public bool timeStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update in Timer.cs called.");
+        if(timeStarted)
+        {
+            Debug.Log("timeStarted is TRUE in update()");
+            timer += Time.deltaTime;
+        }
+        UpdateUi();
+    }
+
+    
+
+    public void StartTimer()
+    {
+        Debug.Log("StartTimer called in Timer.cs");
+        timer = 0;
+        timeStarted = true;
+        UpdateUi();
+    }
+
+    private void UpdateUi()
+    {
+        Debug.Log("UpdateUi called in Timer.cs");
+        timerText.text = FormatTimeTextField();
+    }
+
+    private string FormatTimeTextField()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
