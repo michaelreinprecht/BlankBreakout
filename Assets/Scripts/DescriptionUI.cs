e18aaedcc82be6a467a27b08d0a757a904dc2f0b
michaelreinprecht <michael.reinprecht@students.fhv.at>
2024-12-04 11:05:17 +0100

    Added descriptionUI, Updated GameOverUI, fixed missing Dropdown collider

---
+++ Assets/Scripts/DescriptionUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DescriptionUI : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+    }
+}
